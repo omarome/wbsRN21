c6e39ba3eec0004031526f949289d93f1426b941
omaralm <omar.al-mashhadani@metropolia.fi>
2021-08-24 14:40:08 +0300

    http-c valmis

--- components/List.js
+++ components/List.js
@@ -1,29 +1,10 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import {FlatList} from 'react-native';
-import {baseUrl} from '../utils/variables';
+import {useMedia} from '../hooks/ApiHooks';
 import ListItem from './ListItem';
 
 const List = (props) => {
-  const [mediaArray, setMediaArray] = useState([]);
-
-  useEffect(() => {
-    const loadMedia = async () => {
-      try {
-        const response = await fetch(baseUrl + 'media');
-        const mediaIlmanThumbnailia = await response.json();
-        const kaikkiTiedot = mediaIlmanThumbnailia.map(async (media) => {
-          const response = await fetch(baseUrl + 'media/' + media.file_id);
-          const tiedosto = await response.json();
-          return tiedosto;
-        });
-        setMediaArray(await Promise.all(kaikkiTiedot));
-      } catch (e) {
-        console.log(e.message());
-      }
-    };
-    loadMedia();
-  }, []);
-
+  const {mediaArray} = useMedia();
   console.log('List: mediaArray', mediaArray);
   return (
     <FlatList
---
+++ hooks/ApiHooks.js
@@ -0,0 +1,40 @@
+import {useEffect, useState} from 'react';
+import {doFetch} from '../utils/http';
+import {baseUrl} from '../utils/variables';
+
+const useMedia = () => {
+  const [mediaArray, setMediaArray] = useState([]);
+
+  useEffect(() => {
+    // https://scriptverse.academy/tutorials/js-self-invoking-functions.html
+    (async () => {
+      setMediaArray(await loadMedia());
+    })();
+  }, []);
+
+  const loadMedia = async () => {
+    try {
+      const mediaIlmanThumbnailia = await doFetch(baseUrl + 'media');
+      const kaikkiTiedot = mediaIlmanThumbnailia.map(async (media) => {
+        return await loadSingleMedia(media.file_id);
+      });
+      return Promise.all(kaikkiTiedot);
+    } catch (e) {
+      console.log('loadMedia', e.message);
+    }
+  };
+
+  const loadSingleMedia = async (id) => {
+    try {
+      const tiedosto = await doFetch(baseUrl + 'media/' + id + '1');
+      return tiedosto;
+    } catch (e) {
+      console.log('loadSingleMedia', e.message);
+      return {};
+    }
+  };
+
+  return {mediaArray, loadMedia, loadSingleMedia};
+};
+
+export {useMedia};
