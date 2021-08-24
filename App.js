85f03e6d22aad4e957cfa320b3debcb739388a3a
omaralm <omar.al-mashhadani@metropolia.fi>
2021-08-24 09:11:20 +0300

    describe changes somehow

--- App.js
+++ App.js
@@ -1,21 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
+import {StatusBar} from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Platform, SafeAreaView, StyleSheet, View} from 'react-native';
+import List from './components/List';
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.droidSafeArea}>
+      <View style={styles.container}>
+        <List />
+      </View>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  droidSafeArea: {
+    flex: 1,
+    backgroundColor: '#ccc',
+    paddingTop: Platform.OS === 'android' ? 25 : 0,
   },
 });
+
+export default App;
---
+++ components/ListItem.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {Image, Text, TouchableOpacity, View, StyleSheet} from 'react-native';
+
+const ListItem = ({singleMedia}) => {
+  console.log(singleMedia);
+  return (
+    <TouchableOpacity style={styles.row}>
+      <View style={styles.imagebox}>
+        <Image
+          style={styles.image}
+          source={{uri: singleMedia.thumbnails.w160}}
+        />
+      </View>
+      <View style={styles.textbox}>
+        <Text style={styles.listTitle}>{singleMedia.title}</Text>
+        <Text>{singleMedia.description}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+    padding: 15,
+    marginBottom: 5,
+    backgroundColor: '#eee',
+    borderRadius: 6,
+    flex: 1,
+  },
+  imagebox: {
+    flex: 1,
+  },
+  image: {
+    flex: 1,
+    borderRadius: 6,
+  },
+  textbox: {
+    flex: 2,
+    padding: 10,
+  },
+  listTitle: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    paddingBottom: 15,
+  },
+});
+
+ListItem.propTypes = {
+  singleMedia: PropTypes.object.isRequired,
+};
+
+export default ListItem;
