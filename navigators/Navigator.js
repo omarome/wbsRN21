7a7ffb6e34a0919371566ffdd4758e86ec7ac115
System Administrator <root@87-92-237-19.rev.dnainternet.fi>
2021-09-06 23:14:12 +0300

    avatar

--- navigators/Navigator.js
+++ navigators/Navigator.js
@@ -1,19 +1,37 @@
-import React, { useContext } from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { NavigationContainer } from '@react-navigation/native';
+/* eslint-disable react/prop-types */
+/* eslint-disable react/display-name */
+import React, {useContext} from 'react';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {NavigationContainer} from '@react-navigation/native';
 import Home from '../views/Home';
 import Profile from '../views/Profile';
 import Single from '../views/Single';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Login from '../views/Login';
-import { MainContext } from '../contexts/MainContext';
+import {MainContext} from '../contexts/MainContext';
+import {Icon} from 'react-native-elements';
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
 const TabScreen = () => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({route}) => ({
+        tabBarIcon: ({color, size}) => {
+          let iconName = '';
+          switch (route.name) {
+            case 'Home':
+              iconName = 'home';
+              break;
+            case 'Profile':
+              iconName = 'account-box';
+              break;
+          }
+          return <Icon name={iconName} size={size} color={color} />;
+        },
+      })}
+    >
       <Tab.Screen name="Home" component={Home} />
       <Tab.Screen name="Profile" component={Profile} />
     </Tab.Navigator>
@@ -21,12 +39,18 @@ const TabScreen = () => {
 };
 
 const StackScreen = () => {
-  const { isLoggedIn } = useContext(MainContext);
+  const {isLoggedIn} = useContext(MainContext);
   return (
     <Stack.Navigator>
       {isLoggedIn ? (
         <>
-          <Stack.Screen name="Front" component={TabScreen} />
+          <Stack.Screen
+            name="Front"
+            component={TabScreen}
+            options={{
+              headerShown: false,
+            }}
+          />
           <Stack.Screen name="Single" component={Single} />
         </>
       ) : (
