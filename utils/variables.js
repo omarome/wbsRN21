842d9ce0b6b5319bdb9eba4ed31803c8624fd1e8
System Administrator <root@Omars-Air.home>
2021-09-21 17:45:14 +0300

    player done

--- utils/variables.js
+++ utils/variables.js
@@ -1,5 +1,5 @@
 const baseUrl = 'https://media.mw.metropolia.fi/wbma/';
 const uploadsUrl = baseUrl + 'uploads/';
-const appID = 'omar';
+const appID = 'omaralm';
 
 export {baseUrl, uploadsUrl, appID};
